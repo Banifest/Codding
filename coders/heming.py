e73a546e404d6574156538c69cdadedc1e0993b3
Banifest <banifest@gmail.com>
2017-04-24 22:45:53 +0300

    Добавленны крдер и декодер кодов Хэминга

--- coders/heming.py
+++ coders/heming.py
@@ -1,15 +1,16 @@
-import sys, os, numpy as np, math
-from coders.combinations import GetCombinations
+import math
+import numpy as np
+
+from coders import abstractCoder
 from coders.casts import *
 from coders.exeption import DecodingException
-from coders import abstractCoder
 
 
 class Coder(abstractCoder.Coder):
     arr = []
 
     def __init__(self, info: int):
-        self.lengthAdditional = int(math.log2(info)) + 1
+        self.lengthAdditional = int(math.log2(info - 1) + 2)
         self.lengthInformation = info
         self.lengthTotal = self.lengthInformation + self.lengthAdditional
 
@@ -17,14 +18,13 @@ class Coder(abstractCoder.Coder):
             temp = []
             count = 0
             flag = True
-            for y in range(x):
-                count += 1
+            count = (1 << x) - 1  # Количество символов требуемых для зануления вначале
 
-            for y in range(x):
+            for y in range((1 << x) - 1):
                 temp.append(0)
 
             while count < self.lengthTotal:
-                for y in range(x + 1):
+                for y in range(1 << x):
                     temp.append(1) if flag == True else temp.append(0)
                     count += 1
                     if count >= self.lengthTotal:
@@ -37,16 +37,14 @@ class Coder(abstractCoder.Coder):
 
     def Encoding(self, info: int) -> list:
         temp = IntToBitList(info)
+        if len(temp) < self.lengthInformation:
+            for x in range(self.lengthInformation - len(temp)):
+                temp.append(0)
         temp.reverse()
         code = []
-        """code = [
-            code.append([0]) if math.log2(count + 1) == int(math.log2(count + 1))\
-                else code.append([temp[count - int(math.log2(count)) - 1]])\
-            for count in range(self.lengthTotal)
-            ]
-        """
+
         for count in range(self.lengthTotal):
-            if math.log2(count + 1) == int(math.log2(count + 1)):
+            if math.log2(count + 1) == int(math.log2(count + 1)):  #Проверка числа на степень 2х
                 code.append([0])
             else:
                 code.append([temp[count - int(math.log2(count)) - 1]])
@@ -57,28 +55,22 @@ class Coder(abstractCoder.Coder):
         for x in range(self.lengthAdditional):
             answer[((1 << x)-1)] = backup_info[x]
         return answer
-        """comb_list = GetCombinations(self.lengthAdditional + 1, self.lengthAdditional)
-        count = 0
-        answer = 0
-        for x in comb_list[:self.lengthAdditional]:
-            xor_one_comb = 0
-            temp_info = IntToBitList(info)
-            for y in [temp_info[y] for y in x]:
-                xor_one_comb ^= y
-            answer += (1 << count) * xor_one_comb
-            count += 1
 
-        answer += (info << count)
-        return answer
-"""
 
     def Decoding(self, info: list) -> int:
         code = np.transpose(np.array([[x] for x in info]))
         answer = []
-        status = BitListToInt(list((np.dot(code, self.arr) % 2)[0]))
+        status = list((np.dot(code, self.arr) % 2)[0])
+        status.reverse()
+        status = BitListToInt(status)
         if status != 0:
-            print(status)
-            raise DecodingException()
+            code[0][status - 1] = (code[0][status - 1] + 1) % 2
+            status = BitListToInt(list((np.dot(code, self.arr) % 2)[0]))
+
+            if status != 0:
+                print(status)
+                raise DecodingException("Не удалось успешно исправить обнаруженные ошибки")
+            print("Произошло успешное исправление ошибки")
         for count in range(len(code[0])):
             if  math.log2(count + 1) != int(math.log2(count + 1)):
                 answer.append(code[0][count])
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-import sys, os, numpy as np
-
 from coders import heming
-from coders.combinations import GetCombinations
 from coders.interference import GenInterference
 
 
 if __name__ == '__main__':
     #  print(GetCombinations(4, 3))
     c = 'a'
-    a = heming.Coder(4)
+    a = heming.Coder(15)
+    print(a.Encoding(18801))
+    print(a.Decoding([1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1]))
 #    print(a.Encoding(11))
-    print(a.Decoding(a.Encoding(11)))
-    print(GenInterference(a.Encoding(13), 0.5))
+#    print(a.Encoding(10))
+    #    print(a.Decoding([1,0,1,1,0,0,0]))
+    #    for x in range(31,0,-1):
+    #        print(a.Encoding(x))
+    #        print(a.Decoding(a.Encoding(x)))
+    print(GenInterference(a.Encoding(13), 0.0000000002))
 
 
 else:
