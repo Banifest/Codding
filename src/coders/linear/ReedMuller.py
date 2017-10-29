c69ef5a9e957ac885515eab896efe4cfd766b19b
banifest <banifest@gmail.com>
2017-10-29 19:10:52 +0300

    добавлена операция кодирования в код рида мюллера

--- src/coders/linear/ReedMuller.py
+++ src/coders/linear/ReedMuller.py
@@ -8,16 +8,27 @@ from coders.casts import BitListToInt, IntToBitList
 
 class Coder(abstractCoder.Coder):
     matrix_G: np.matrix
+    vectors: list
+    vectors_rise: list = []
+    r: int
+    power: int
 
     def __init__(self, power: int, r: int):
-        init_matrix: list = [IntToBitList((1 << (1 << power)) - 1)]
-        init_matrix += np.matrix([IntToBitList(x, size=power) for x in range(2 ** power)]).T.tolist()
-        matrix_int_G1 = [BitListToInt(x) for x in init_matrix[1:]]
+        self.r = r
+        self.power = power
 
-        for x in range(r):
+        init_matrix: list = [IntToBitList((1 << (1 << power)) - 1)]
+        if r > 0:
+            self.vectors = np.matrix([IntToBitList(x, size=power, rev=False) for x in range(2 ** power)]).T.tolist()
+            init_matrix += self.vectors
+            matrix_int_G1 = [BitListToInt(x) for x in init_matrix[1:]]
+            self.vectors_rise = [[x] for x in range(1, power + 1)]
+
+        for x in range(r - 1):
             comb: list = list(itertools.combinations(range(power), x + 2))
 
             new_matrix_G: list = []
+            self.vectors_rise += comb
             for x in comb:
                 val: int = matrix_int_G1[x[0]]
                 for y in x:
@@ -26,3 +37,23 @@ class Coder(abstractCoder.Coder):
             init_matrix += new_matrix_G
 
         self.matrix_G = np.matrix(init_matrix)
+
+    def Encoding(self, information: list):
+        return [x % 2 for x in (np.matrix(information) * self.matrix_G).tolist()[0]]
+        pass
+
+    def Decoding(self, information: list):
+        vec_sum = lambda a, b: [a[x] ^ b[x] for x in range(len(a))]
+        vec_mul = lambda a, b: [a[x] & b[x] for x in range(len(a))]
+        vec_inv = lambda a: [x ^ 1 for x in a]
+
+        for x in range(len(self.matrix_G), 0, -1):
+            voice: int = 0
+            for option_mul in range(1 << len(self.vectors_rise[x])):
+                pass
+            pass
+
+        pass
+        #
+        # [1111011011011000]
+        # [1111101001101100]
