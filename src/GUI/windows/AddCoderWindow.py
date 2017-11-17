e5144031a0343b9e64613a2916567a1bb27bd589
banifest <banifest@gmail.com>
2017-11-17 22:05:59 +0300

    добавлена вспомогательная информация о кодерах
    Signed-off-by: banifest <banifest@gmail.com>

--- src/GUI/windows/AddCoderWindow.py
+++ src/GUI/windows/AddCoderWindow.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QIntValidator
-from PyQt5.QtWidgets import QGridLayout, QLabel, QLineEdit, QRadioButton, QWidget
+from PyQt5.QtWidgets import QLabel, QLineEdit, QRadioButton, QWidget
 from PyQt5.uic import loadUi
 
 from src.logger import log
@@ -24,15 +24,15 @@ class AddCoderWindow(QWidget):
         self.controller = controller
         loadUi(r'src\GUI\UI\adding_coder.ui', self)
 
-        self.sizePackageTextBox: QLineEdit = QLineEdit()
-        self.listPolynomialTextBox: QLineEdit = QLineEdit()
-        self.countPolynomialTextBox: QLineEdit = QLineEdit()
-        self.countMemoryRegistersTextBox: QLineEdit = QLineEdit()
-        self.countExitsTextBox: QLineEdit = QLineEdit()
-        self.sizeBlockTextBox: QLineEdit = QLineEdit()
-        self.countBlocksTextBox: QLineEdit = QLineEdit()
-        self.powerReedMullerTextBox = QLineEdit()
-        self.maxLengthRecoverPackageTextBox = QLineEdit()
+        self.sizePackageTextBox: QLineEdit = QLineEdit("120")
+        self.listPolynomialTextBox: QLineEdit = QLineEdit("3, 5, 7")
+        self.countPolynomialTextBox: QLineEdit = QLineEdit("3")
+        self.countMemoryRegistersTextBox: QLineEdit = QLineEdit("3")
+        self.countExitsTextBox: QLineEdit = QLineEdit("2")
+        self.sizeBlockTextBox: QLineEdit = QLineEdit("10")
+        self.countBlocksTextBox: QLineEdit = QLineEdit("30")
+        self.powerReedMullerTextBox = QLineEdit("2")
+        self.maxLengthRecoverPackageTextBox = QLineEdit("3")
 
         self.sizePackageTextBox.setValidator(QIntValidator())
         # self.listPolynomialTextBox.setValidator(QIntValidator())
@@ -49,7 +49,7 @@ class AddCoderWindow(QWidget):
 
         self.show()
 
-    def redraw_grid(self) -> QGridLayout:
+    def redraw_grid(self) -> None:
         for i in reversed(range(self.grid_coder_options.count())):
             self.grid_coder_options.itemAt(i).widget().setParent(None)
 
@@ -62,9 +62,11 @@ class AddCoderWindow(QWidget):
             self.grid_coder_options.addWidget(QLabel("Размер пакета"), 2, 0)
             self.grid_coder_options.addWidget(self.sizePackageTextBox, 2, 1)
             self.grid_coder_options.addWidget(QLabel("Порождающий полином"), 3, 0)
+            self.listPolynomialTextBox: QLineEdit = QLineEdit("13")
             self.grid_coder_options.addWidget(self.listPolynomialTextBox, 3, 1)
         elif text == "Сверточный":
             self.grid_coder_options.addWidget(QLabel("Список полиномов"), 3, 0)
+            self.listPolynomialTextBox: QLineEdit = QLineEdit("3, 5, 7")
             self.grid_coder_options.addWidget(self.listPolynomialTextBox, 3, 1)
             self.grid_coder_options.addWidget(QLabel("Количество регистров памяти"), 4, 0)
             self.grid_coder_options.addWidget(self.countMemoryRegistersTextBox, 4, 1)
