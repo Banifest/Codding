36ec170c83fed7711509fe812968857e2e61fc21
Unknown <banifest@gmail.com>
2018-02-27 12:50:01 +0300

    restore .ignore

---
+++ main.py
@@ -0,0 +1,14 @@
+from src.GUI.controller.MainController import MainController
+
+from src.logger import log
+
+if __name__ == '__main__':
+    try:
+        log.info("Start program")
+        controller = MainController()
+        log.info("End program")
+    except Exception as e:
+        print(e)
+        log.critical("Unhandled exception")
+else:
+    raise Exception("Невозможен import данного файла:(")
