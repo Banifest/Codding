3165249e4a9a92613938e2f8f90eaa18ed4dd074
Banifest <banifest@gmail.com>
2018-11-08 21:23:15 +0300

    Database entities mirror changes

--- src/statistics/db/coder.py
+++ src/statistics/db/coder.py
@@ -5,6 +5,8 @@ from src.statistics.db.entity import Entity
 
 
 class Coder(Entity):
+    TABLE_NAME: str = "Coder"
+
     class CodersType(Enum):
         HEMMING = auto()
         CYCLICAL = auto()
@@ -19,9 +21,29 @@ class Coder(Entity):
     len_additional_information: int
     interleaver: bool
 
+    def __init__(
+            self,
+            guid,
+            coder_description,
+            type_of_coder,
+            coder_speed,
+            len_input_information,
+            len_additional_information,
+            interleaver
+    ):
+        super()
+        self.guid = guid
+        self.coder_description = coder_description
+        self.type_of_coder = type_of_coder
+        self.coder_speed = coder_speed
+        self.len_input_information = len_input_information
+        self.len_additional_information = len_additional_information
+        self.interleaver = interleaver
+
+
     def create(self):
         self._connection.query("""
-                    INSERT INTO Coder(guid, coder_description, type_of_coder, coder_speed, len_input_information, len_additional_information, interleaver)
+                    INSERT INTO {7}(guid, coder_description, type_of_coder, coder_speed, len_input_information, len_additional_information, interleaver)
                     VALUES ({0}, {1}, {2}, {3}, {4}, {5}, {6});
                 """.format(
             self.guid,
@@ -30,12 +52,13 @@ class Coder(Entity):
             self.coder_speed,
             self.len_input_information,
             self.len_additional_information,
-            self.interleaver
+            self.interleaver,
+            Coder.TABLE_NAME
         ))
 
     def delete(self):
         self._connection.query("""
-            DELETE FROM Coder
+            DELETE FROM {7}
             WHERE guid = {0}
               AND coder_description = {1}}
               AND type_of_coder = {2}}
@@ -50,11 +73,12 @@ class Coder(Entity):
             self.coder_speed,
             self.len_input_information,
             self.len_additional_information,
-            self.interleaver
+            self.interleaver,
+            Coder.TABLE_NAME
         ))
 
-    def read(self):
-        self._prepare_selection_statement()
+    def read(self) -> list:
+        return self._prepare_selection_statement(Coder.TABLE_NAME)
 
     def save(self):
         pass
