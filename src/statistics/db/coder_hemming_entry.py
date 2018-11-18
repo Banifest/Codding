a2be826512bcb07b1c433470bcdeb69e5b9cbacb
banifest <banifest@gmail.com>
2018-11-19 00:55:04 +0300

    Add hemming entity

--- src/statistics/db/coder_hemming_entry.py
+++ src/statistics/db/coder_hemming_entry.py
@@ -1,74 +1,48 @@
-from enum import Enum, auto
 from uuid import UUID
 
-from src.statistics.db.entity import Entity
+from src.statistics.db.coder_entry import CoderEntry
 
 
-class CoderEntry(Entity):
-    TABLE_NAME: str = "Coder"
-
-    class CodersType(Enum):
-        HEMMING = auto()
-        CYCLICAL = auto()
-        CONVOLUTION = auto()
-        FOUNTAIN = auto()
-
-    guid: UUID
-    coder_description: str
-    type_of_coder: CodersType
-    coder_speed: int
-    len_input_information: int
-    len_additional_information: int
-    interleaver: bool
+class CoderHemmingEntry(CoderEntry):
+    matrix_of_transformation: list
 
     def __init__(
             self,
             guid: UUID,
             coder_description: str,
-            type_of_coder: CodersType,
+            type_of_coder: CoderEntry.CodersType,
             coder_speed: int,
             len_input_information: int,
             len_additional_information: int,
-            interleaver: bool
+            interleaver: bool,
+            matrix_of_transformation: list
     ):
-        super().__init__()
-        self.guid = guid
-        self.coder_description = coder_description
-        self.type_of_coder = type_of_coder
-        self.coder_speed = coder_speed
-        self.len_input_information = len_input_information
-        self.len_additional_information = len_additional_information
-        self.interleaver = interleaver
+        super().__init__(
+            guid,
+            coder_description,
+            type_of_coder,
+            coder_speed,
+            len_input_information,
+            len_additional_information,
+            interleaver
+        )
+        self.matrix_of_transformation = matrix_of_transformation
 
     def create(self):
+        super().create()
+
         self._connection.query(f"""
-                    INSERT INTO "Coder"(
-                        "GUID", 
-                        description, 
-                        coder_type, 
-                        coder_speed, 
-                        input_length, 
-                        additional_length, 
-                        interleaver
+                    INSERT INTO "Hemming"(
+                        coder, 
+                        matrix
                     )  VALUES (
                     '{self.guid}',
-                    '{self.coder_description}', 
-                    {self.type_of_coder.value}, 
-                    {self.coder_speed}, 
-                    {self.len_input_information}, 
-                    {self.len_additional_information}, 
-                    {self.interleaver}
+                    {self.matrix_of_transformation}                     
                     )
                 """)
 
     def delete(self):
-        self._connection.query(f"""
-            DELETE FROM "Coder"
-            WHERE "GUID" = {self.guid};
-        """)
+        super().delete()
 
     def read(self) -> list:
-        return self._connection.query(f"""
-        SELECT *
-        FROM {Coder.TABLE_NAME};        
-    """)
+        return super().read()
