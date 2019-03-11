bb5b46c97d2a80e8ea2998c79c5354134723e285
Banifest <banifest@gmail.com>
2019-03-11 20:41:24 +0300

    Split functional from coder controller

--- src/endpoint/general_chanel_simulate.py
+++ src/endpoint/general_chanel_simulate.py
@@ -1,17 +1,13 @@
 # coding=utf-8
-# coding=utf-8
-from numpy import deprecate
-
 from src.GUI.controller.cascade_coder_test_thread import CascadeCoderTestThread
 from src.GUI.controller.coder_controller import CoderController
 from src.GUI.controller.single_coder_test_thread import SingleCoderTestThread
 from src.channel.enum_noise_mode import EnumNoiseMode
 from src.helper.error.error_handler import ErrorHandler
 from src.helper.error.exception.application_exception import ApplicationException
-from src.helper.error.exception.parameters_parse_exception import ParametersParseException
 
 
-class TestController:
+class GeneralChanelSimulate:
     """
     Class provide functionality for control testing process
     """
@@ -42,73 +38,8 @@ class TestController:
             self,
             first_coder_params: CoderController,
             second_coder_params: CoderController,
-    ):
-        self._firstCoderParams = first_coder_params
-        self._secondCoderParams = second_coder_params
-
-    def set_quantity_steps_in_test_cycle(self, value: int) -> None:
-        self._quantityStepsInTestCycle = value
-
-    def set_flg_first_interleaver(self, value: bool) -> None:
-        self._flgFirstInterleaver = value
-
-    def set_flg_second_interleaver(self, value: bool) -> None:
-        self._flgSecondInterleaver = value
-
-    def set_length_first_interleaver(self, value: int) -> None:
-        self._lengthFirstInterleaver = value
-
-    def set_length_second_interleaver(self, value: int) -> None:
-        self._lengthSecondInterleaver = value
-
-    def set_noise_package_period(self, value: int) -> None:
-        self._packagePeriod = value
-
-    def set_noise_single_mode(self, value: bool) -> None:
-        if value:
-            self._noiseMode = EnumNoiseMode.SINGLE
-
-    def set_noise_package_mode(self, value: bool) -> None:
-        if value:
-            self._noiseMode = EnumNoiseMode.PACKAGE
-
-    def set_noise_mix_mode(self, value: bool) -> None:
-        if value:
-            self._noiseMode = EnumNoiseMode.MIX
-
-    def set_noise_package_length(self, value: int) -> None:
-        self._noisePackageLength = value
-
-    def set_is_split_package(self, value: bool) -> None:
-        self._flgSplitPackage = value
-
-    def set_noise_start(self, value: float) -> None:
-        self._noiseStart = value
-
-    def set_noise_end(self, value: float) -> None:
-        self._noiseEnd = value
-
-    def set_count_test(self, value: int) -> None:
-        self._countTest = value
-
-    def set_test_info(self, value: str) -> None:
-        try:
-            self._testInfo = int(value)
-        except ValueError as rcx_value_error:
-            ErrorHandler().gui_message_box(
-                rcx_exception=ParametersParseException(
-                    message=ParametersParseException.INFORMATION_NOT_SERIALIZABLE.message,
-                    long_message=ParametersParseException.INFORMATION_NOT_SERIALIZABLE.long_message,
-                    previous=rcx_value_error
-                )
-            )
-
-    @deprecate
-    def set_mode_cascade(self, value: str) -> None:
-        if value == 'First':
-            self._mode = 0
-        elif value == 'Second':
-            self._mode = 1
+    ) -> None:
+        super().__init__()
 
     def set_first_coder_thread_class(self):
         self._firstThreadClass = SingleCoderTestThread(
